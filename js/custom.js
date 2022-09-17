cda7cf4056d3663d7fb1f5cc4731292ba85ee14e
KonaYukii <amyli07cd@gmail.com>
2022-09-17 15:50:54 -0400

    Banner and Heading Changed

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@
     var nav = $($anchor.attr('href'));
     if (nav.length) {
       $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
+        scrollTop: $($anchor.attr('href')).offset().top - 90
       }, 1100, 'easeInOutExpo');
 
       event.preventDefault();
@@ -34,7 +34,7 @@
     }
     let scrollDistance = $(window).scrollTop();
     $('section').each(function(i) {
-      if ($(this).position().top - 50 <= scrollDistance) {
+      if ($(this).position().top - 140 <= scrollDistance) {
         $('.navbar-nav li').removeClass('active');
         $('.navbar-nav li').eq(i).addClass('active');
       }
